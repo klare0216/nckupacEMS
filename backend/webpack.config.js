96d2ac3f7dce025700cf1d1326f325b54fae6ba8
Wei-Cheng Teng <ca9479@gmail.com>
2018-09-18 14:04:46 +0800

    environment setting

---
+++ backend/webpack.config.js
@@ -0,0 +1,30 @@
+/* webpack.config.js ： Webpack 的設定檔 */
+
+const nodeExternals = require('webpack-node-externals');
+const path = require('path');
+
+module.exports = {
+	target: 'node',
+	node: {
+		__dirname: false,
+		__filename: false,
+	},
+	externals: [nodeExternals()],
+	entry: {
+		'index': './src/index.js',
+	},
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: '[name].bundle.js',
+		libraryTarget: 'commonjs2',
+	},
+	module: {   //設定你的檔案選項
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: 'babel-loader'
+			},
+		],
+	},
+};
